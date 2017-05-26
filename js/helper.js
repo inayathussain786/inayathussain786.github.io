53f38072656944297853776bb13ee776f23c57f6
inayathussain786 <sih.25011993@gmail.com>
2017-05-26 17:33:02 +0530

    commit

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,3 @@
-/*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
@@ -58,10 +44,6 @@ var HTMLonlineURL = '<br><a href="#">%data%</a>';
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
-
-/*
-The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
 $(document).ready(function() {
 	$('button').click(function() {
 		var $name = $('#name');
@@ -70,9 +52,6 @@ $(document).ready(function() {
 	});
 });
 
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in the lesson Flow Control from JavaScript Basics.
-*/
 var clickLocations = [];
 
 function logClicks(x,y) {
@@ -93,14 +72,7 @@ $(document).click(function(loc) {
 	logClicks(x,y);
 });
 
-
-
-/*
-This is the fun part. Here's where we generate the custom Google Map for the website.
-See the documentation below for more details.
-*/
-var map;    // declares a global map variable
-//Start here! initializeMap() is called when page is loaded.
+var map;
 function initializeMap() {
 
 	var locations;
@@ -108,37 +80,19 @@ function initializeMap() {
 	var mapOptions = {
 		disableDefaultUI: true
 	};
-	/*
-	For the map to be displayed, the googleMap var must be
-	appended to #mapDiv in resumeBuilder.js.
-	*/
+
 	map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
-
-	/*
-	locationFinder() returns an array of every location string from the JSONs
-	written for bio, education, and work.
-	*/
 	function locationFinder() {
 
-		// initializes an empty array
 		var locations = [];
 
-		// adds the single location property from bio to the locations array
 		locations.push(bio.contacts.location);
 
-		// iterates through school locations and appends each location to
-		// the locations array. Note that forEach is used for array iteration
-		// as described in the Udacity FEND Style Guide:
-		// https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
 		education.schools.forEach(function(school){
 			locations.push(school.location);
 		});
 
-		// iterates through work locations and appends each location to
-		// the locations array. Note that forEach is used for array iteration
-		// as described in the Udacity FEND Style Guide:
-		// https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
 		work.jobs.forEach(function(job){
 			locations.push(job.location);
 		});
@@ -146,29 +100,19 @@ function initializeMap() {
 		return locations;
 	}
 
-	/*
-	createMapMarker(placeData) reads Google Places search results to create map pins.
-	placeData is the object returned from search results containing information
-	about a single location.
-	*/
 	function createMapMarker(placeData) {
 
-		// The next lines save location data from the search result object to local variables
 		var lat = placeData.geometry.location.lat();  // latitude from the place service
 		var lon = placeData.geometry.location.lng();  // longitude from the place service
 		var name = placeData.formatted_address;   // name of the place from the place service
 		var bounds = window.mapBounds;            // current boundaries of the map window
 
-		// marker is an object with additional data about the pin for a single location
 		var marker = new google.maps.Marker({
 			map: map,
 			position: placeData.geometry.location,
 			title: name
 		});
 
-		// infoWindows are the little helper windows that open when you click
-		// or hover over a pin on a map. They usually contain more information
-		// about a location.
 		var infoWindow = new google.maps.InfoWindow({
 			content: name
 		});
